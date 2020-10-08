6a1b2fb1a4ba765dcfd69beee598574928252aa3
Benjamin <1234hteimaj4321@gmail.com>
2020-10-07 20:10:43 -0600

    added js to actually work

--- desktop/app.js
+++ desktop/app.js
@@ -1,18 +1,22 @@
-const images = document.querySelectorAll(".wallpaper-container img");
-const aspectRatioContainers = document.querySelectorAll(
-  ".aspect-ratio-container"
-);
+// swapped to query selectors, also changed what they were selecting, most of the code is changed
 
-aspectRatioContainers.forEach((item) => {
-  let img = item.previousElementSibling;
-  console.log(img.width);
-  item.style.width = img.width + "px";
-});
+const images = document.querySelectorAll(".wallpaper-container img");
+
 images.forEach((item) => {
   item.addEventListener("mouseover", (e) => {
+    const aspectRatioContainers = document.querySelectorAll(
+      ".aspect-ratio-container"
+    );
+    aspectRatioContainers.forEach((item) => {
+      const img = item.previousElementSibling;
+      console.log(img.width);
+      item.style.width = img.width + "px";
+    });
     item.nextElementSibling.classList.add("visible");
   });
   item.addEventListener("mouseout", (e) => {
     item.nextElementSibling.classList.remove("visible");
   });
 });
+
+// end of changed code
