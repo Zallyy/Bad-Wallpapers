c139153e3d78b4f5d4432a65efd10524980d50ba
Benjamin <1234hteimaj4321@gmail.com>
2020-10-09 15:03:01 -0600

    added the correct js files to each html file and hooked up everything, other than 'displaying the full image' link

--- desktop/js/app.js
+++ desktop/js/app.js
@@ -1,21 +1,16 @@
-// swapped to query selectors, also changed what they were selecting, most of the code is changed
-
 const images = document.querySelectorAll(".wallpaper-container img");
 
-images.forEach((item) => {
-  item.addEventListener("mouseover", (e) => {
-    const aspectRatioContainers = document.querySelectorAll(
-      ".aspect-ratio-container"
-    );
-    aspectRatioContainers.forEach((item) => {
-      const img = item.previousElementSibling;
-      console.log(img.width);
-      item.style.width = img.width + "px";
-    });
-    item.nextElementSibling.classList.add("visible");
+// renamed item to img
+images.forEach((img) => {
+  img.addEventListener("mouseover", (e) => {
+    //removed aspect ratio selector
+
+    //this is selecting the aspect ratio container
+    img.parentElement.nextElementSibling.style.width = 200 + "px";
+    img.parentElement.nextElementSibling.classList.add("visible");
   });
-  item.addEventListener("mouseout", (e) => {
-    item.nextElementSibling.classList.remove("visible");
+  img.addEventListener("mouseout", (e) => {
+    img.parentElement.nextElementSibling.classList.remove("visible");
   });
 });
 
