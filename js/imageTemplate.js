772ad0efcf873c22b840719b65923d3a10c39225
Zallyy <Zallybags@gmail.com>
2020-10-09 00:01:09 -0700

    all functionally wired up

--- js/imageTemplate.js
+++ js/imageTemplate.js
@@ -10,11 +10,13 @@ let dateEl = document.getElementById('date')
 let wallpaperEl = document.getElementById('wallpaper')
 let tagsEl = document.getElementById('tags')
 let aspectRatioEl = document.getElementById('aspect-ratio')
+let fullsize = document.getElementById('full-size')
 
 function CreatePage ({image, aspectRatio, tags, dateAdded}) {
     dateEl.innerHTML = dateAdded
     wallpaperEl.src = image 
     aspectRatioEl.innerHTML = aspectRatio
+    fullsize.href = image;
     tags.forEach(tag => {
         tagsEl.innerHTML += `
         <button class="tag">${tag}</button>
